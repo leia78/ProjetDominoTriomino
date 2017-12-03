f12827b679e145800177837d815e7a33f58b1494
leia78 <34215780+leia78@users.noreply.github.com>
2017-12-03 18:57:52 +0100

    Add files via upload

---
+++ structures.h
@@ -0,0 +1,26 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+
+typedef struct{
+    int x;
+    int y;
+}position;
+
+typedef struct{
+    int *number;
+    position pos;
+}piece;
+
+piece *pieces;
+char jeu;
+
+void accueil (void);
+
+#endif
