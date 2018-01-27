98aaf7ad3e2b250004a66ac2818839e08a500be3
leia78 <34215780+leia78@users.noreply.github.com>
2018-01-27 14:24:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,21 +7,21 @@ int main (void)
 	SDL_Event event;
 
 	SDL_Init(SDL_INIT_EVERYTHING); 
+	ecran = SDL_SetVideoMode(LECRAN, HECRAN, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 	
 	while(continuer)
 	{
 		SDL_WaitEvent(&event);
 		
 		switch(event.type) // On teste le type d'événement.
-        {
-            case SDL_QUIT:
-                continuer = 0;
-                break;
-            default:
-                ecran = SDL_SetVideoMode(LECRAN, HECRAN, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-                accueil(ecran);
-                break;
-        }
+        	{
+		    case SDL_QUIT:
+			continuer = 0;
+			break;
+		    default:
+			accueil(ecran);
+			break;
+		}
 	}
 	
 	SDL_Quit();
